65a65f9e124483cf8500c4fa30809cd83e675a62
Baptiste_Granier <samueldesaix@gmail.com>
2025-11-28 00:04:43 +0100

    Ajout plot ok

---
+++ Nextflow/working_nf/Deseq_2811/plot/data/deseq_fonc.R
@@ -0,0 +1,57 @@
+
+
+args <- commandArgs(trailingOnly = TRUE)
+counts  <- args[1]   # counts.txt
+
+#1 
+library(DESeq2)
+library(ggplot2)
+# Lire le counts.txt
+counts_raw <- read.table(counts, header=TRUE, sep="\t",
+                         comment.char="#", stringsAsFactors=FALSE, check.names=FALSE)
+
+# Colonnes des counts
+start_col <- 7
+count_matrix <- counts_raw[, start_col:ncol(counts_raw)]
+
+# Gene IDs comme noms de lignes
+rownames(count_matrix) <- counts_raw$Geneid
+
+# Convertir en matrice entière
+count_matrix <- as.matrix(apply(count_matrix, 2, as.integer))
+
+# Créer coldata
+sample_names <- colnames(count_matrix)
+coldata <- data.frame(
+  row.names = sample_names,
+  condition = factor(c("persister","persister","persister",
+                       "control","control","control"))
+)
+
+
+# Créer l'objet DESeqDataSet
+dds <- DESeqDataSetFromMatrix(countData = count_matrix,
+                              colData = coldata,
+                              design = ~ condition)
+
+# Filtrer les gènes avec peu de counts (au total < 10)
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep,]
+
+
+#3
+# Lancer l'analyse DESeq2
+dds <- DESeq(dds)
+
+# Obtenir les résultats (padj < 0.05)
+res <- results(dds, alpha=0.05)
+
+# Trier les gènes par p-value ajustée
+resOrdered <- res[order(res$padj),]
+
+#4
+# MA-plot de tous les gènes
+png("MAplot_all_genes.png", width=800, height=600)
+plotMA(res, ylim=c(-4,4), main="MA-plot (all genes)")
+plot.window(xlim = c(0, 1e6), ylim = c(-4, 4), log = "x")
+dev.off()
---
+++ Nextflow/working_nf/Deseq_2811/plot/main2.nf
@@ -0,0 +1,22 @@
+nextflow.enable.dsl=2
+
+process deseq2 {
+    tag "Running"
+    container 'vmichelet/r341_desq2'
+
+    input:
+    path counts
+    file deseq_script
+
+    output:
+    path "*.png"
+
+    script:
+    """
+    Rscript ${deseq_script} ${counts}
+    """
+}
+
+workflow {
+    deseq2(file("counts.txt"), file("data/deseq_fonc.R") )
+}
