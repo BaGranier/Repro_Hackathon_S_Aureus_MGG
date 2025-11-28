c49a6f783f6eebe3b5d8cc00aad8ac70805cd01f
Baptiste_Granier <samueldesaix@gmail.com>
2025-11-28 01:16:22 +0100

    Ajout de la figure type papier

--- Nextflow/working_nf/Deseq_2811/plot/data/deseq_fonc.R
+++ Nextflow/working_nf/Deseq_2811/plot/data/deseq_fonc.R
@@ -62,7 +62,53 @@ resOrdered <- res[order(res$padj),]
 
 #4
 # MA-plot de tous les gènes
-png("MAplot_all_genes.png", width=800, height=600)
-plotMA(res, ylim=c(-4,4), main="MA-plot (all genes)")
-plot.window(xlim = c(0, 1e6), ylim = c(-4, 4), log = "x")
+# png("MAplot_all_genes.png", width=800, height=600)
+# plotMA(res, ylim=c(-4,4), main="MA-plot (all genes)")
+# plot.window(xlim = c(0, 1e6), ylim = c(-4, 4),  log = "x")
+# dev.off()
+
+
+
+png("MAplot_custom.png", width=1000, height=800)
+
+df <- as.data.frame(res)
+df <- df[df$baseMean <= 1e6, ]
+
+# ---- Config ----
+ylim_min <- -4
+ylim_max <- 4
+
+# couleur DESeq2-like
+df$col <- ifelse(df$padj < 0.05, "red", "black")
+
+# ---- Séparer les 3 catégories ----
+df_inside  <- df[df$log2FoldChange <= ylim_max & df$log2FoldChange >= ylim_min, ]
+df_top     <- df[df$log2FoldChange >  ylim_max, ]
+df_bottom  <- df[df$log2FoldChange <  ylim_min, ]
+
+# ---- Plot ----
+ggplot() +
+    # Points "normaux"
+    geom_point(data = df_inside,
+               aes(x = baseMean, y = log2FoldChange, color = col),
+               alpha = 0.6, size = 1) +
+
+    # Triangles du haut
+    geom_point(data = df_top,
+               aes(x = baseMean, y = ylim_max, color = col),
+               shape = 24, size = 2, fill = "white") +     # triangle haut
+
+    # Triangles du bas
+    geom_point(data = df_bottom,
+               aes(x = baseMean, y = ylim_min, color = col),
+               shape = 25, size = 2, fill = "white") +     # triangle bas
+
+    scale_color_identity() +
+    scale_x_log10(limits = c(1, 1e6)) +
+    ylim(ylim_min, ylim_max) +
+    ggtitle("MA-plot with triangles for out-of-range values") +
+    xlab("baseMean (log10 scale)") +
+    ylab("log2 Fold Change")
+
 dev.off()
+
